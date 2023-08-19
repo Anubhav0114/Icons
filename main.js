7674e15525a0a7903633bf1b229e4f384dde02b1
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-20 01:12:40 +0530

    font-awsome and flowbite

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello");
 const fs_1 = __importDefault(require("fs"));
 const folderNames = [
     "Accessibility",
@@ -95,3 +94,42 @@ function generateFileNames(path) {
 }
 // generateFileNames('./temp/icons')
 // function generate
+function prefix3(path, prefix) {
+    const fileName = fs_1.default.readdirSync(path);
+    fileName.forEach(element => {
+        const newName = prefix + element;
+        const oldpath = path + "/" + element;
+        const newPath = path + "/" + newName;
+        fs_1.default.renameSync(oldpath, newPath);
+    });
+}
+prefix3("./pawan/flowbite/solid/arrows/", "fs-");
+prefix3("./pawan/flowbite/solid/brands/", "fs-");
+prefix3("./pawan/flowbite/solid/e-commerce/", "fs-");
+prefix3("./pawan/flowbite/solid/emoji/", "fs-");
+prefix3("./pawan/flowbite/solid/file-folders/", "fs-");
+prefix3("./pawan/flowbite/solid/general/", "fs-");
+prefix3("./pawan/flowbite/solid/media/", "fs-");
+prefix3("./pawan/flowbite/solid/text/", "fs-");
+prefix3("./pawan/flowbite/solid/user/", "fs-");
+prefix3("./pawan/flowbite/solid/weather/", "fs-");
+prefix3("./pawan/flowbite/stroke/arrows/", "fs-");
+prefix3("./pawan/flowbite/stroke/e-commerce/", "fs-");
+prefix3("./pawan/flowbite/stroke/emoji/", "fs-");
+prefix3("./pawan/flowbite/stroke/file-folders/", "fs-");
+prefix3("./pawan/flowbite/stroke/general/", "fs-");
+prefix3("./pawan/flowbite/stroke/media/", "fs-");
+prefix3("./pawan/flowbite/stroke/text/", "fs-");
+prefix3("./pawan/flowbite/stroke/user/", "fs-");
+prefix3("./pawan/flowbite/stroke/weather/", "fs-");
+// np
+function removePrefix(path, prefix) {
+    const removeFileName = fs_1.default.readdirSync(path);
+    removeFileName.forEach(element => {
+        const newFileName = element.replace(prefix, "");
+        const oldpath = path + "/" + element;
+        const newPath = path + "/" + newFileName;
+        fs_1.default.renameSync(oldpath, newPath);
+    });
+}
+// removePrefix("./pawan/font-awesome/brands/","fs-fs");
