ecf71de2023db0522762326266e1d5bef8ead647
sayamsharma <ssayam200@gmail.com>
2023-08-20 18:23:03 +0530

    outline added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Hello World")
 import os
-
+import shutil
 # Specify the path to the directory you want to list files from
 directory_path = './sayam/ionicons'
 
@@ -14,18 +14,17 @@ for file_name in file_names:
     outline = file_name.endswith('outline.svg')
     sharp = file_name.endswith('sharp.svg')
     if outline:
-        os.rename("./sayam/ionicons/"+file_name,'./sayam/outline')
+        shutil.move("./sayam/ionicons/"+file_name,'./sayam/fill/')
         outCount = outCount+1
         print("outline:- "+file_name)
         print("Moved")
-    else:
-        print("not outline")
     if sharp:
+        shutil.move("./sayam/ionicons/"+file_name,'./sayam/stroke/')
         sharpCount = sharpCount+1
         print("sharp:- "+file_name)
     else:
         print("not sharp")
 
-print(file_names)
+# print(file_names)
 print(outCount)
-print(sharpCount)
+# print(sharpCount)
